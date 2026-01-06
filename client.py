fe72f4470b0dd37e36852d439fdb30fbe63af935
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2026-01-06 23:47:32 +0200

    client can now choose between post and get method (post method still doesnt work with caching)

--- client.py
+++ client.py
@@ -1,9 +1,27 @@
 import requests
 
 
-name = input("Enter person's name pls: ")
+choice = input(
+        """
+what request you want to do?
+"post" for a post request,
+and "get" for a get request: 
+        """
+        )
 
-response = requests.get(f"http://localhost/getage/{name}")
+while choice.lower() != 'q':
+    if choice.lower() == "get":
+        name = input("Enter person's name pls: ")
 
-print(response.text)
+        response = requests.get(f"http://localhost/getage/{name}")
 
+        print(response.text)
+    elif choice.lower() == "post":
+        name = input("Enter person's name pls: ")
+
+        data = {
+                "name": name
+        }
+
+        response = requests.post(f"http://127.0.0.1:8000/checkonperson", json=data)
+        print(response.text)
