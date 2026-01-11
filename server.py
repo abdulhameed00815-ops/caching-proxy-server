cf5aba5d02f4aac760d2e0017da34e65be624ef6
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2026-01-11 22:53:16 +0200

    now clear cache works + added rate limiter for requests (untested + requires more comments)

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+import time
+from typing import Callable, Any
+from functools import wraps
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request, Depends, HTTPException, Response
 from sqlalchemy import PrimaryKeyConstraint, create_engine, Column, String, Integer, LargeBinary, DateTime
 from sqlalchemy.dialects.postgresql import JSONB
@@ -47,8 +50,39 @@ def get_db_requests():
         db.close()
 
 
+def rate_limit(max_calls: int, period: int):
+    def decorator(func: Callable[[request], Any]) -> Callable[[Request], Any]:
+        usage: dict[str, list[float]] = {}
+
+        @wraps(func)
+        async def wrapper(request: Request) -> Any:
+            if not request.client:
+                raise ValueError("Request has no client information")
+            ip_address: str = request.client.host
+
+            now = time.time()
+            if ip_address not in usage:
+                usage[ip_address] = []
+            timestamps = usage[ip_address]
+            timestamps[:] = [t for t in timestamps if now - t < period]
+
+            if len(timestamps) < max_calls:
+                timestamps.append(now)
+                return await func(request)
+             
+            wait = period - (now - timestamps[0])
+            raise HTTPException(
+                    status_code=429,
+                    detail=f"Rate limit exceeded, Retry after {wait:.2f} seconds",
+            )
+
+        return wrapper
+
+    return decorator
+
+
 def clear_cache():
-    session_requests.query(Request).delete(synchronize_session=False)
+    session_requests.query(CachedRequest).delete(synchronize_session=False)
     session_requests.commit()
     print("cache cleared successfuly")
 
@@ -56,17 +90,15 @@ def clear_cache():
 # this is a generic route that accepts all kinds of restful requests (proxy), we make a request to it, then the route extracts the path "/{full_path:path}", and that ":path" is just for extracting the 
 # "/"s too, then we forward the path to our origin, then all the rest is just caching logic (this is all for get methods), 
 @fastapi.api_route("/{full_path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
+@rate_limit(max_calls=10, period=30)
 async def proxy(full_path: str, request: Request):
-    url = f"http://127.0.0.1:8000/{full_path}" 
+    if full_path == "clear_cache":
+        clear_cache()
+        return {"message": "cache cleared successfuly"}
+    else:
+        url = f"http://127.0.0.1:8000/{full_path}" 
 
-    current_time = datetime.now()
-    five_minutes_ago = current_time - timedelta(minutes=5)
-    expired_requests = list(session_requests.query(CachedRequest).filter(CachedRequest.date < five_minutes_ago).all())
-    for cached_request in expired_requests:
-        session_requests.delete(cached_request)
-        session_requests.commit()
 
-    if request.method == "GET":
         cached_url = session_requests.query(CachedRequest.url).filter(CachedRequest.url == url).scalar()
         if cached_url:
             cached_response = session_requests.query(CachedRequest.response).filter(CachedRequest.url == url).scalar()
@@ -85,24 +117,23 @@ async def proxy(full_path: str, request: Request):
             session_requests.add(new_request)
             session_requests.commit()
             return {"response": resp.text}
-    elif request.method == "POST":
-        cached_url = session_requests.query(CachedRequest.url).filter(CachedRequest.url == url).scalar()
-        cached_body = session_requests.query(CachedRequest.body).filter(CachedRequest.body == await request.json()).scalar()
-        if cached_url and cached_body:
-            cached_response = session_requests.query(CachedRequest.response).filter(CachedRequest.url == url).scalar()
-            return {"response": cached_response}
-        else: 
-            async with httpx.AsyncClient() as client:
-                resp = await client.request(
-                    method=request.method,
-                    url=url,
-                    headers=request.headers.raw,
-                    content=await request.body()    
-                )
+        elif request.method == "POST":
+            cached_url = session_requests.query(CachedRequest.url).filter(CachedRequest.url == url).scalar()
+            cached_body = session_requests.query(CachedRequest.body).filter(CachedRequest.body == await request.json()).scalar()
+            if cached_url and cached_body:
+                cached_response = session_requests.query(CachedRequest.response).filter(CachedRequest.url == url).scalar()
+                return {"response": cached_response}
+            else: 
+                async with httpx.AsyncClient() as client:
+                    resp = await client.request(
+                        method=request.method,
+                        url=url,
+                        headers=request.headers.raw,
+                        content=await request.body()    
+                    )
 
-            body = await request.json()
-            new_request = CachedRequest(url=url, body=body, type=request.method, response=resp.text)
-            session_requests.add(new_request)
-            session_requests.commit()
-            return {"response": resp.text}
-
+                body = await request.json()
+                new_request = CachedRequest(url=url, body=body, type=request.method, response=resp.text)
+                session_requests.add(new_request)
+                session_requests.commit()
+                return {"response": resp.text}
